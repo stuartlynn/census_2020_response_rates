085287e1c5e5f4b24f775e2198ffe32d22d90d93
Stuart Lynn <stuart.lynn@gmail.com>
2020-03-23 20:48:56 +0000

    download scripts for 2020 geoms and releationship file

---
+++ code/download_2020_tracts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -o ../data/geo/sr20_detailed.zip https://www2.census.gov/geo/maps/DC2020/SR20/sr20_detailed.zip
---
+++ code/download_tract_relationship_db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -o ../data/geo/rr_tract_rel.zip https://www2.census.gov/geo/maps/DC2020/SR20/rr_tract_rel.zip
+
